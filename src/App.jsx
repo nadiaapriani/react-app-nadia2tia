e9da14096db49d39230ee147f4bcde189d6de418
nadiaapriani <nadia23ti@mahasiswa.pcr.ac.id>
2025-05-21 16:20:07 +0700

    ttt

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,12 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       
       </div>
-      <h1>Vite + hub</h1>
+      <h1>Vite + github</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count isg {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
